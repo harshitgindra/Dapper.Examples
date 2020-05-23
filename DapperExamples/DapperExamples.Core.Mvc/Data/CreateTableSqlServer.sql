3b122b4616ad644df3c96ac0418368aa2d4f6150
Harshit Gindra <harshitgindra@gmail.com>
2020-05-23 14:46:11 -0500

    Adding sql script to create table in sql server

---
+++ DapperExamples/DapperExamples.Core.Mvc/Data/CreateTableSqlServer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[AppUser]  (
+                 Id                                 integer primary key identity(1,1),
+                 FirstName                           nvarchar(100) not null,
+                 LastName                            nvarchar(100) not null,
+                 Age                         int not null
+              )
